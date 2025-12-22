63aef53ef4c501f8e1a1999436489de9d1d53819
Kyler Chin <kyler@catenarymaps.org>
2025-12-21 19:34:11 -0800

    astro config rendering bug fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import svelte from '@astrojs/svelte';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [svelte(), react()],
 
   vite: {
     plugins: [tailwindcss()]
